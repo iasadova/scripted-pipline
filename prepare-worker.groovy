462f9c4237786f4b7d73c0529d68fe882bcb54cf
Iroda1986 <iroda20.1986@gmail.com>
2020-09-04 15:28:20 -0500

    add change

--- prepare-worker.groovy
+++ prepare-worker.groovy
@@ -1,8 +1,24 @@
-node ('master'){
-    stage ('init') {
-         withCredentials([sshUserPrivateKey(credentialsId: 'jenkins-master-key', keyFileVariable: 'SSHKEY', passphraseVariable: '', usernameVariable: 'SSHUSERNAME')]) {
-            sh 'ssh -o StrictHostKeyChecking=no -i $SSHKEY  $SSHUSERNAME@157.245.214.206 yum install epel-release -y '
-        }
-    }
-}
+properties([
+  parameters([string(defaultValue: '', description: 'Please enter VM IP ', name: 'nodeIP', trim: true)])])
 
+if (nodeIP.trim()) {
+       node {
+   withCredentials([sshUserPrivateKey(credentialsId: 'jenkins-master-key', keyFileVariable: 'key', passphraseVariable: '', usernameVariable: 'username')]) {
+     stage('init'){
+           sh 'ssh -o  StrictHostKeyChecking=no -i $key root@${nodeIP} yum install epel-release -y '
+        }
+     stage('install git'){
+           sh 'ssh -o  StrictHostKeyChecking=no -i $key root@${nodeIP} yum install git -y '
+           
+        }
+     stage('install java'){
+           sh 'ssh -o  StrictHostKeyChecking=no -i $key root@${nodeIP} yum install java-1.8.0-openjdk-devel -y'
+          
+        }
+      }
+     
+  } 
+}
+else {
+   error 'Please enter valid IP address'
+}
