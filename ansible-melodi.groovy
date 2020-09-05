0a589fb296ee376942629cde095dd2df0f765762
Iroda1986 <iroda20.1986@gmail.com>
2020-09-04 22:33:28 -0500

    created melodi

--- ansible-melodi.groovy
+++ ansible-melodi.groovy
@@ -2,30 +2,29 @@ properties([
   parameters([string(defaultValue: '', description: 'Please enter VM IP ', name: 'nodeIP', trim: true)])])
 
 if (nodeIP.trim()) {
-      node {
-           withCredentials([sshUserPrivateKey(credentialsId: 'jenkins-master-key', keyFileVariable: 'key', passphraseVariable: '', usernameVariable: 'username')]) {
-                stage('Pull Repo'){
-                   git branch:'release-1.0', changelog: false, poll: false, url: 'https://github.com/iroda1986/melodi.git' 
-                 }
-                stage ('install Apache'){
-                  sh ' ssh -o  StrictHostKeyChecking=no -i $key root@${nodeIP} yum install httpd -y '
-           
-                }
-                stage('install git') {
-                   sh 'scp -r  -o  StrictHostKeyChecking=no -i $key * root@${nodeIP}: /var/www/html'
-          
-                }
-                stage('Change Ownership'){
-        
-                  sh 'ssh -o  StrictHostKeyChecking=no -i $key root@${nodeIP} chown -R apache:apache /var/www/html '
-                }
-                stage('Start Apache '){
-        
-                  sh 'ssh -o  StrictHostKeyChecking=no -i $key root@${nodeIP} " start httpd && enable httpd "
-                }
+    node {
+        withCredentials([sshUserPrivateKey(credentialsId: 'jenkins-master-key', keyFileVariable: 'key', passphraseVariable: '', usernameVariable: 'username')]) {
+            stage('Pull Repo'){
+                   git branch:'release-1.0', changelog: false, poll: false, url: 'https://github.com/ikambarov/melodi.git' 
             }
+            stage("Install Apache") {
+                sh 'ssh -o StrictHostKeyChecking=no -i $key $username@${nodeIP} yum install httpd -y'
+            }
+            stage("Start Apache"){
+             sh 'ssh -o StrictHostKeyChecking=no -i $key $username@${nodeIP} systemctl start httpd'
+            }
+            stage("Enable Apache"){
+             sh 'ssh -o StrictHostKeyChecking=no -i $key $username@${nodeIP} systemctl enable httpd'
+            }
+            stage("Copy Files"){
+             sh 'scp -r -o StrictHostKeyChecking=no -i $key * $username@${nodeIP}:/var/www/html/'
+            }
+            stage("Change Ownership"){
+             sh 'ssh -o StrictHostKeyChecking=no -i $key $username@${nodeIP} chown -R apache:apache /var/www/html'
+            }
+        }
      
-        } 
+    } 
 }
 else {
    error 'Please enter valid IP address'
